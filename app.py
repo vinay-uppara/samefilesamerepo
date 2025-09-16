f7313a5fa410fb5d105156282367ac6d12e63fd1
uppara <upparavinay2003n@gmail.com>
2025-09-16 16:40:18 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# In-memory list to store tasks (clears when the server restarts)
+todo_list = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=todo_list)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form.get('task')
+    if task:
+        todo_list.append(task)
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:task_id>')
+def delete(task_id):
+    if 0 <= task_id < len(todo_list):
+        todo_list.pop(task_id)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
